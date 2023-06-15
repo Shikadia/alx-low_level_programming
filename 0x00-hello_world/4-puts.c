a886213f28d80c69dab78405c75d8cb215e70854
shikadia <otukachinedu661@gmail.com>
2023-06-15 05:39:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - the entry point of the code.
+* return - the return type in this case 0
+* /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
