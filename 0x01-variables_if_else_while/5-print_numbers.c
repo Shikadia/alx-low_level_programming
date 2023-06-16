815c58c3c8b6d375b9ec14f0e5fb781653aa6228
shikadia <otukachinedu661@gmail.com>
2023-06-16 14:47:14 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point of programme, print all numbers
+* Return: returns 0
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
