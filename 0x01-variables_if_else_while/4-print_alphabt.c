88a69df6b8287ea1943370d504dba9d971206e02
shikadia <otukachinedu661@gmail.com>
2023-06-16 14:01:29 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - entry point of programme, print all lower case
+* character
+* Return: returns 0
+*/
+int main(void)
+{
+	int charValue = 97;
+
+	while (charValue < 123)
+	{
+		if (charValue != 101 && charValue != 113)
+		{	putchar(charValue);
+			charValue++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
