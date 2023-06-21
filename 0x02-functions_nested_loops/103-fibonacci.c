88dbeba8d4ed4847ca4cd37f31296ace2609f435
shikadia <otukachinedu661@gmail.com>
2023-06-20 17:08:59 -0700

    103 fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the sum of the even-valued terms
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	long num = 0;
+	long x = 0;
+	long y = 1;
+	long sum = 0;
+
+	while (num < 4000000)
+	{
+		sum = x + y;
+		printf("%ld", sum);
+
+		x = y;
+		y = sum;
+
+		if (num == 3800000)
+			printf("\n");
+		else
+			printf(", ");
+		num++;
+	}
+}
