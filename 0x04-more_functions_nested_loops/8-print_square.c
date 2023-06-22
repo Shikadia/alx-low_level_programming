86de55be844c0aff2e1d7821ef7ad49517c9b20f
shikadia <otukachinedu661@gmail.com>
2023-06-22 13:24:11 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line on the terminal
+ * @size: The number of times the character # should be printed
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	x = 0;
+	while (x < size)
+	{
+		y = 0;
+		while (y < size)
+		{
+			_putchar('#');
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
