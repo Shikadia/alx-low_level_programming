e43620828c39d097a51f221b5fd46e4a4f68c966
shikadia <otukachinedu661@gmail.com>
2023-06-22 12:20:40 -0700

    nu

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
