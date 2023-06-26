1ca6fdc963d471b8295072cbf46f49c9c122a881
shikadia <otukachinedu661@gmail.com>
2023-06-26 01:34:36 -0700

    0.98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Takea a pointer to an address
+* and derefference it
+* @n: pointer to an address wth a value
+* Return: returns void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
