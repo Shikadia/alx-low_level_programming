1b67f2ba8e327868f4aa17d41522460fbcdaaabc
shikadia <otukachinedu661@gmail.com>
2023-07-07 12:21:19 -0700

    add missed file to fix task 0 error

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* *_strncpy - Copies a string frm @src to @desc buffer
+* @dest: The pointer to the destination buffer to copy to
+* @src: The source string to copy from
+* @n: The number of characters to copy
+* Return: Returns a pointer to the resulting string dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strcmp - Compares two strings
+* @s1: Pointer to the first string to be compared
+* @s2: Pointer to the Second string to be compared
+*
+* Return: If *s1 == *s2, return 0
+*	   If *s1 != *s2, return the string difference (*s1 - *s2)
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (0);
+}
