a24be394a254bb16553c568a88f1cbed8e6db3fa
root <root@chinedO1>
2023-07-18 07:30:07 +0100

    description for struts

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  * struct dog - A created for an entity dog
+  * @name - dog's name
+  * @age - dog's age
+  * @owner - dog's owner
+  */
 struct dog
 {
 	char *name;
@@ -10,6 +16,9 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+  * dog_t - a new struct defined called dog_t
+  */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
