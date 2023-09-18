1c02bbb98626842977e39e466294d9f86f5e5cf3
shikadia <otukachinedu661@gmail.com>
2023-09-17 18:02:04 -0700

    Fix code output fail

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes;
 
-    nodes = 0;
+	nodes = 0;
 	while (h)
 	{
 		nodes++;
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,7 +10,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes;
 
-    nodes = 0;
+	nodes = 0;
 	while (h)
 	{
 		nodes++;
